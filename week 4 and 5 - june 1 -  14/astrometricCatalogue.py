5b6fdd2227e0174676d442ce07de1563a3964411
Prashansa Gupta <prashansa17793@gmail.com>
2015-06-11 15:38:20 +0200

    adding the functional program now

--- week 4 and 5 - june 1 -  14/astrometricCatalogue.py
+++ week 4 and 5 - june 1 -  14/astrometricCatalogue.py
@@ -5,7 +5,9 @@ from esutil.coords import sphdist
 import healpy as hp
 
 #load data into a variable
+t0 = time()
 testFile= np.sort(np.load('/home/gupta/projects/propermotion/test.npy'))
+print time() - t0
 
 #save unique galaxy ids in another variable
 uniqueObjIDfileTemp = np.unique(testFile['obj_id'])
@@ -19,7 +21,7 @@ noOfYears = 5
 #the width of the pixel -- if user defined
 pixelWidth = 3 #in arcminutes
 #the distance upto which the galaxies are included for updating ra/dec
-searchRadius = 2#in arcminutes
+searchRadius = 0.28#in arcminutes
 #the number of observations actually available for each object
 #noOfObs = np.zeros(len(uniqueObjIDfile)) 
 
@@ -39,7 +41,7 @@ searchRadius = 2#in arcminutes
 
 
 #this code pixelizes the dataset
-nside= 2**10
+nside= 2**5
 
 #nside2npix gives number of pixels for a given nside, create an array containing pixel indices  
 pixelIndexArray= np.arange(hp.nside2npix(nside))
@@ -50,10 +52,14 @@ theta, phi = hp.pix2ang(nside, pixelIndexArray)
 pixelRa = 180*phi/np.pi
 pixelDec = 90 - theta*180/np.pi
 
+#calculate phi and theta for ra/dec values of the testFile
+phiForObj = (testFile['ra'] * np.pi)/180 
+thetaForObj = (90 - testFile['dec'])* (np.pi/180)
+pixelIndexForObj= hp.ang2pix(nside,thetaForObj, phiForObj)
 
-#pick a pixel
-pickPixelNo = pixelIndexArray[len(pixelIndexArray)/2]
 
+#pick a pixel from the testFile
+pickPixelNo = pixelIndexForObj[len(pixelIndexForObj)/2]
 
 #find objects within the searchRadius and whose noOfObs is atleast 3
 
@@ -61,105 +67,202 @@ pickPixelNo = pixelIndexArray[len(pixelIndexArray)/2]
 #VERSION 1
 #--------------------
 
+#take those objects whose atleast one value of ra/dec is inside the searchRadius
 angSepMask1 =  sphdist( pixelRa[pickPixelNo], pixelDec[pickPixelNo], testFile['ra'], testFile['dec'] ) <= (searchRadius*60)
 
+#separate these entire rows from the original testFile
 searchFile = testFile[angSepMask1]
 
-noOfObs = np.zeros(len(np.unique(searchFile))) 
+#putting unique object IDs in this file
+uniqueSearchFile = np.unique(searchFile['obj_id'])
 
-medianRaArray=np.ones((np.unique(searchFile)).size)
-medianDecArray=np.ones((np.unique(searchFile)).size)
+#file containing data for objects inside pixel
+objInPixelFile = testFile[pixelIndexForObj == pickPixelNo]
+
+#file containing unique objIDs of the pixel
+uniqueObjInPixelFile = np.unique(objInPixelFile['obj_id'])
+
+#for containing no of observations for each object 
+noOfObs = np.ones(len(uniqueSearchFile))
+
+#for containing median ra/dec values for each object
+medianRaArray=np.zeros(uniqueSearchFile.size)
+medianDecArray=np.zeros(uniqueSearchFile.size)
 
 #stores the residual values for each object and for each epoch
 residualRaArray= np.ones(len(searchFile))
 residualDecArray= np.ones(len(searchFile))
-
-currentObjID = searchFile['obj_id'][0]
-raObj = [searchFile['ra'][0]]
-decObj = [searchFile['dec'][0]]
-mjdObj = [searchFile['mjd'][0]]
-
-
-t0 = time()
-objIDcounter = 0
-pos=0
 
 mjdSorted = np.sort(searchFile['mjd'])
 deltaT = mjdSorted[0:-1] - mjdSorted[1:]
 tBreakAt = np.where(deltaT < (-100.0))[0]
 mjdBreakAt = mjdSorted[tBreakAt+1]
 
-    #it should start from the second row, so add one.
-for i in  np.arange(searchFile.size-1)+1:#np.arange(100)+1:#
-    print i
+currentObjID = searchFile['obj_id'][0]
+raObj = [searchFile['ra'][0]]
+decObj = [searchFile['dec'][0]]
+mjdObj = [searchFile['mjd'][0]]
+
+t1 = time()
+objIDcounter = 0
+pos=0
+
+#it should start from the second row, so add one.
+for i in  np.arange(searchFile.size-1)+1:#np.arange(10)+1:#
+    #print i
     objID = searchFile['obj_id'][i]
     ra = searchFile['ra'][i]
     dec = searchFile['dec'][i]
     mjd = searchFile['mjd'][i]
-    print ra
+    #print objID
+    #print ra
+    #print dec
+    #print mjd
     if (objID == currentObjID):
         mjdObj.append(mjd)
         raObj.append(ra)
         decObj.append(dec)
-        print 'same object now'
+        #print 'same object now'
         noOfObs[objIDcounter]+=1
-    elif (noOfObs[objIDcounter] >= 3.0):
-        #convert list into array
-        raObj=np.array(raObj)
-        decObj=np.array(decObj)
-        #calculate and store medians
-        medianRa = np.median(raObj)
-        medianDec = np.median(decObj)
-        print 'medianRa is', medianRa
-        print 'medianDec is', medianDec
-        #median value is for each object
-        medianRaArray[objIDcounter] = medianRa
-        medianDecArray[objIDcounter] = medianDec
-        print 'we are on the', objIDcounter,'object'
-        print 'no of obs', noOfObs[objIDcounter]
-        #calculate residual
-        residualRa = np.array(raObj) - medianRa
-        residualDec = np.array(decObj) - medianDec
-        #store in separate arrays
-        #print 'residualRa is', residualRa
-        #print 'residualDec is', residualDec
-        #print 'pos is', pos
-        #print 'i is',i
-        residualRaArray[pos:i] = residualRa
-        residualDecArray[pos:i] = residualDec
+    else:
+        #print 'different object now'
+        #print 'we are on the', objIDcounter,'object'
+        if(noOfObs[objIDcounter] >= 3.0):
+            #print 'no of obs >= 3.0', noOfObs[objIDcounter]
+            #convert list into array
+            raObj=np.array(raObj)
+            decObj=np.array(decObj)
+            #calculate and store medians
+            medianRa = np.median(raObj)
+            medianDec = np.median(decObj)
+            #print 'medianRa is', medianRa
+            #print 'medianDec is', medianDec
+            #median value is for each object
+            medianRaArray[objIDcounter] = medianRa
+            medianDecArray[objIDcounter] = medianDec
+            #calculate residual
+            residualRa = np.array(raObj) - medianRa
+            residualDec = np.array(decObj) - medianDec
+            #store in separate arrays
+            #print 'residualRa is', residualRa
+            #print 'residualDec is', residualDec
+            #print 'pos is', pos
+            #print 'i is',i
+            residualRaArray[pos:i] = residualRa
+            residualDecArray[pos:i] = residualDec
         objIDcounter +=1
         currentObjID = objID
         pos = i
         raObj = [searchFile['ra'][i]]
         decObj = [searchFile['dec'][i]]
         mjdObj = [searchFile['mjd'][i]]
-        print 'different object now'
-print time()-t0
+print 'final time is',time() - t1
 
-#residualRaEpochWise = np.zeros( (len(searchFile),len(mjdBreakAt+1)) )
-#residualDecEpochWise = np.zeros( (len(searchFile),len(mjdBreakAt+1)) )
-medianResidualRaEpochWise = np.zeros(len(mjdBreakAt+1) )
-medianResidualDecEpochWise = np.zeros(len(mjdBreakAt+1) )
+#residualRaEpochWise = np.zeros( (len(searchFile),len(mjdBreakAt)+1) )
+#residualDecEpochWise = np.zeros( (len(searchFile),len(mjdBreakAt)+1) )
+medianResidualRaEpochWise = np.zeros(len(mjdBreakAt)+1)
+medianResidualDecEpochWise = np.zeros(len(mjdBreakAt)+1)
 
 #if there are atleast three epochs present
 if (len(mjdBreakAt)>=3.0):
     previousVar2 = np.min(mjdSorted)
     for var1, var2 in enumerate(mjdBreakAt, start=0):
-        mjdIndex = (searchFile['mjd']<= var2) & (previousVar2 <= searchFile['mjd'])
+        mjdIndex = (searchFile['mjd']< var2) & (previousVar2 < searchFile['mjd'])
+        mjdIndexInPixel = (objInPixelFile['mjd']<var2) & (previousVar2 < objInPixelFile['mjd'])
         print var1
         if any(mjdIndex):
-            print residualRaArray[mjdIndex]
+            #print 'hehe'
+            #print residualRaArray[mjdIndex]
             #residualRaEpochWise[:, var1] = residualRaArray[mjdIndex]
             medianResidualRaEpochWise[var1] = np.median(residualRaArray[mjdIndex])
             medianResidualDecEpochWise[var1] = np.median(residualDecArray[mjdIndex])
+            #replacing old values with new values
+            objInPixelFile['ra'][mjdIndexInPixel] -=  medianResidualRaEpochWise[var1]
+            objInPixelFile['dec'][mjdIndexInPixel] -=  medianResidualDecEpochWise[var1]
         else:
+            #print 'haha'
+            #print residualRaArray[mjdIndex]
             medianResidualRaEpochWise[var1+1] = np.median(residualRaArray[mjdIndex])
             medianResidualDecEpochWise[var1+1] = np.median(residualDecArray[mjdIndex])
+            #replacing old values with new values
+            objInPixelFile['ra'][mjdIndexInPixel] -=  medianResidualRaEpochWise[var1+1]
+            objInPixelFile['dec'][mjdIndexInPixel] -=  medianResidualDecEpochWise[var1+1]
+            #newRa = objInPixelFile['ra'][mjdIndexInPixel] - medianResidualRaEpochWise[var1+1]
+            #newDec = objInPixelFile['dec'][mjdIndexInPixel] - medianResidualDecEpochWise[var1+1]
         previousVar2 = var2
 
 
+#calculate final ra/dec as the median of newRa/newDec values
+finalRaArray = np.zeros(uniqueObjInPixelFile.size)
+finalDecArray = np.zeros(uniqueObjInPixelFile.size)
 
-#JUNK 
+currObj = objInPixelFile['obj_id'][0]
+raObj = [objInPixelFile['ra'][0]]
+decObj = [objInPixelFile['dec'][0]]
+mjdObj = [objInPixelFile['mjd'][0]]
+
+counter = 0
+t2 = time()
+for i in np.arange(objInPixelFile.size-1)+1:#np.arange(10)+1: #
+    objID = objInPixelFile['obj_id'][i]
+    ra = objInPixelFile['ra'][i]
+    dec = objInPixelFile['dec'][i]
+    if(objID == currObj):
+        raObj.append(ra)
+        decObj.append(dec)
+    else:
+        raObj=np.array(raObj)
+        decObj=np.array(decObj)
+        finalRaArray[counter] = np.median(raObj)
+        finalDecArray[counter] = np.median(decObj)
+        # print finalRaArray[counter]
+        #print i
+        #print counter
+        currObj = objID
+        raObj = [objInPixelFile['ra'][i]]
+        decObj = [objInPixelFile['dec'][i]]
+        counter +=1
+print time()-t2
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+#find objects in the pixel of interest -- give pixel indices to all the objects -- pixel indices act like bins -- use these pixel indices to match the index of the pixel in question and VOILA! you have all the ra/dec values of the objects inside the pixel
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+#JUNK CODE
 #-----------------------------------------------------------------------------------------------------
 
 objIDin1 =np.unique(testFile['obj_id'][angSepMask1])
@@ -184,6 +287,17 @@ testFileIn = testFile[m2]
 
 
 residualRaArray[]
+
+
+#calculate phi and theta given ra and dec
+phiForObj = (searchFile['ra'] * np.pi)/180 
+thetaForObj = (90 - searchFile['dec'])* (np.pi/180)
+pixelIndexForObj= hp.ang2pix(nside,thetaForObj, phiForObj)
+
+phiForObj = (testFile['ra'] * np.pi)/180 
+thetaForObj = (90 - testFile['dec'])* (np.pi/180)
+pixelIndexForObj= hp.ang2pix(nside,thetaForObj, phiForObj)
+
 
 
 #angSepMask =  sphdist( pixelRa[pickPixelNo], pixelDec[pickPixelNo],pixelRa, pixelDec ) <= (searchRadius*60)
